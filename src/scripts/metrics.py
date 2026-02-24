38469e84bc2176d93b1dfd1e2b1e88c08a370cf7
humza_0237 <74483186+Hamza-Ali0237@users.noreply.github.com>
2026-02-24 21:04:56 +0500

    Implemented "get_metrics" Function
    - Dice Score & Mean IoU
    - Might modify this class in the future.

---
+++ src/scripts/metrics.py
@@ -0,0 +1,14 @@
+import torchmetrics
+
+def get_metrics(num_classes: int, device):
+
+    metrics = torchmetrics.MetricCollection({
+        'dice_score': torchmetrics.segmentation.DiceScore(
+            num_classes, include_background=True, average="micro", input_format="one_hot"
+        ),
+        "miou": torchmetrics.segmentation.MeanIoU(
+            num_classes, include_background=True, per_class=False, input_format="one_hot"
+        )
+    })
+
+    return metrics
